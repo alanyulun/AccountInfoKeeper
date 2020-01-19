55f388caadc607faf84a9f4faca33bf7bb2c405b
于倫 邱 <alan123456alan789@gmail.com>
2020-01-19 15:37:46 +0800

    新增Class_SQLiteCommunication

--- AccountInfoKeeper/Class_SQLiteCommunication.cs
+++ AccountInfoKeeper/Class_SQLiteCommunication.cs
@@ -7,13 +7,63 @@ namespace AccountInfoKeeper
 {
     class Class_SQLiteCommunication
     {
-        SQLiteConnection Connection;
-        SQLiteCommand Command;
-        SQLiteDataReader Reader;
+        static string DBPath = @".\Resource\DB\ITCG_IDirector_DB.sqlite";
+        static string ConnStr = "data source=" + DBPath;
+        public SQLiteConnection Connection;
 
         public Class_SQLiteCommunication()
         {
+            Connection = new SQLiteConnection(ConnStr);
+        }
 
+        //寫入資料庫
+        public int SQLWrite(SQLiteCommand SQLCommand)
+        {
+
+            Connection.Open();
+            int QuantityOfChangeData = SQLCommand.ExecuteNonQuery();
+            Connection.Close();
+            return QuantityOfChangeData;
+        }
+
+        public List<string[]> SQLRead(SQLiteCommand SQLCommand)
+        {
+            Connection.Open();
+            SQLiteDataReader myReader = SQLCommand.ExecuteReader();
+
+            List<string[]> Raws = new List<string[]>();
+            if (myReader.HasRows)
+            {
+                while (myReader.Read())
+                {
+                    string[] RawDataList = new string[myReader.FieldCount];
+                    for (int i = 0; i < RawDataList.Length; i++)
+                        RawDataList[i] = myReader[i].ToString();
+                    Raws.Add(RawDataList);
+                }
+            }
+            else
+                Raws = null;
+
+            Connection.Close();
+            return Raws;
+        }
+
+        public bool IsDataExist(string TableName, string ID)
+        {
+            bool HasRows = false;
+
+            SQLiteCommand SQLCommand = new SQLiteCommand("SELECT * FROM " + TableName + " WHERE [ID]=@ID", Connection);
+            SQLCommand.Parameters.AddWithValue("@ID", ID);
+            Connection.Open();
+            SQLiteDataReader myReader = SQLCommand.ExecuteReader();
+            if (myReader.HasRows)
+                HasRows = true;
+            else
+                HasRows = false;
+
+            Connection.Close();
+            return HasRows;
         }
     }
 }
